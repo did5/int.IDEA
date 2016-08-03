82bedee1a52c76923908b807b7bba05ab08a6c95
Баранов Артем Александрович <0908@vnipiet.ru>
2016-08-03 08:50:14 +0300

    1

---
+++ src/Fibonacci.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by 0908 on 03.08.2016.
+ */
+public class Fibonacci {
+}
---
+++ src/FizzBuzz.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by 0908 on 02.08.2016.
+ */
+public class FizzBuzz {
+}
