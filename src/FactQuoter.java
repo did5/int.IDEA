8f6b5b92226bfff4449657f5e28d67388afe263e
Баранов Артем Александрович <0908@vnipiet.ru>
2016-08-05 14:28:29 +0300

    1

---
+++ src/FactQuoter.java
@@ -0,0 +1,23 @@
+/**
+ * Пример из книги Java в примерах. Справочник. Дэвид Флэнаган
+ * Интерактивный ввод. Стр.37
+ */
+import java.io.*;
+
+public class FactQuoter {
+    public static void main (String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        for (;;){
+            System.out.print("FactQuoter> ");
+            String line = in.readLine();
+            if ((line == null) || (line.equals("quit"))) break;
+            try{
+                int x = Integer.parseInt(line);
+                System.out.println(x + "! = " + Factorial4.factorial(x));
+            }
+            catch(Exception e) {
+                System.out.println("Invalid Input");
+            }
+        }
+    }
+}
